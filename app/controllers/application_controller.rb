7bd1afa878d4660cf7f8e9f44bbe07a6a52ec7df
Ariel I <arielibarra39@gmail.com>
2020-10-22 15:41:41 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class App < Sinatra::Base
   post "/teams" do 
     @teams = Team.new(params[:team])
     
-    params[:team][:hero]
+    params[:team][:hero].each do |details|
+      details
     
     erb :team 
   end 
